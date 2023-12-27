4b17891947b26a8bb4282ec62b15a1f2d203a5c7
Santhoshbharadwaj_H_A <santhoshbharadwajha@gmail.com>
2023-12-27 14:04:59 +0530

    five hour complted

---
+++ objectsJS.js
@@ -0,0 +1,9 @@
+// objects group the multiple value together 
+
+const objectName= {
+    names : "socks",
+    price : 1010
+//property:value
+}
+console.log(objectName)
+console.log(objectName.names)

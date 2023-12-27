c16c4ec4fd2a2b46f53f17694f353602ad902f0b
Santhoshbharadwaj_H_A <santhoshbharadwajha@gmail.com>
2023-12-27 21:41:05 +0530

    eight hour completed

---
+++ conversion.js
@@ -0,0 +1,2 @@
+let a = "10" // converting the string to num
+let num = Number(a)
---
+++ windowsObject.js
@@ -0,0 +1,5 @@
+// imp:-  window is a built in object that represen the browser
+
+window.console.log("window"); //it is samea as
+
+console.log("windows") // javascript automatically adds the windows before the funcction

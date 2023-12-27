06fc5201eade3befa39d54c4f29392de20efd585
Santhoshbharadwaj_H_A <santhoshbharadwajha@gmail.com>
2023-12-27 20:54:50 +0530

    seven hour completed

---
+++ builtInObjects.js
@@ -0,0 +1,18 @@
+// most important built in objects are json and local storage
+
+// Json Built in object, JSON(javascript object notation)
+
+// all properties-values must use double quotes
+//json doesnot support  single qoutes and function 
+
+
+// converting the Javascript object into the json
+
+console.log(JSON.stringify(objectNames))
+
+JSONstring = JSON.stringify(objectNames)
+
+// converting the JSON to javascript object
+
+JSON.parse(JSONstring)
+
--- other.js
+++ other.js
@@ -9,3 +9,11 @@ console.log("sometime we uses the pascle case")
 //Scope of the variable
 
 // if you create a  variable inside the the curly braket it is restricted to that scope --> local scope
+
+
+
+// difference between null and undefiend
+
+//null -> intentionally want some parameter to be empty
+
+// always remember that if you combine the "string and a number javascript automatically combines it as the string"
